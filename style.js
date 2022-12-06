930bee84ef038ee5b16e4d5479a9412ea65950a6
quang nguyễn <Nguyen Quang>
2022-12-07 01:33:12 +0700

    add file

---
+++ style.js
@@ -0,0 +1,45 @@
+function clickOpenlogin() {
+    document.getElementById("login").style.display="flex";
+}
+
+function clickCloselogin(){
+    document.getElementById("login").style.display="none";
+}
+
+function clickOpenregister() {
+    document.getElementById("register").style.display="flex";
+}
+
+function clickCloseregister(){
+    document.getElementById("register").style.display="none";
+}
+
+function clickOpenheader() {
+    document.getElementById("header").style.display="flex";
+}
+
+function clickCloseheader() {
+    document.getElementById("header").style.display="none";
+}
+
+function clickOpentextsearch() {
+    document.getElementById("text-search.").style.display="flex";
+}
+
+function  clickCloseiconsearch() {
+    document.getElementById("icon-search.").style.display="none";
+}
+
+// const showForUser = document.querrySelectAll('.js-show')
+// const loginOpen = document.querrySelector('.js-login')
+// const registerOpen =document.querrySelecter('.js-register')
+
+// function showpages() {
+//     // loginOpen.classList.add('open')
+//     // registerOpen.classList.add('open')
+//     alert("show")
+// }
+
+// for (const show of showForUser) {
+//     show.addEvenListener('click', showpages)
+// }
